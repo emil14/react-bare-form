bf4cbf03e41f618c18e89219f918d01a05cfbb58
emil14 <emil.musician@gmail.com>
2018-05-18 03:17:19 +0300

    Update component
    - Rerwite from function to PureComponent with static propTypes
    - Implement data-flow

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Form = props => {
-  const { content, ...rootProps } = props
-  const children = content.map(child => {
+class Form extends React.PureComponent {
+  static propTypes = {
+    content: PropTypes.arrayOf(PropTypes.object).isRequired,
+    model: PropTypes.objectOf(PropTypes.string).isRequired,
+  }
+
+  handleChange = (key, value, handler) => {
+    const upd = { [key]: value }
+    handler && handler(upd)
+    this.props.onChange && this.props.onChange(upd)
+  }
+
+  mapContentToChildren = child => {
+    const { model } = this.props
     const {
       element: Element,
       name,
       options = [],
       label: labelText,
       text,
+      onChange: onChangeLocal,
       className = 'form',
       ...elementProps
     } = child
-    const body =
-      Element === 'select'
-        ? options.map(optionProps => <option {...optionProps} key={optionProps.value}>{optionProps.label}</option>)
-        : text
+    const body = Element === 'select'
+      ? options.map(optionProps => <option {...optionProps} key={optionProps.value}>{optionProps.label}</option>)
+      : text
     return (
       <React.Fragment key={name}>
-        {labelText && <label for={name}>{labelText}</label>}
+        {labelText && <label htmlFor={name}>{labelText}</label>}
         <Element
           name={name}
+          value={model[name]}
+          onChange={e => this.handleChange(name, e.target.value, onChangeLocal)}
           className={`${className}__${Element} ${className}__${Element}--${name}`}
           {...elementProps}
         >
@@ -29,15 +42,18 @@ const Form = props => {
         </Element>
       </React.Fragment>
     )
-  })
-  return <form {...rootProps}>{children}</form>
+  }
+
+  render = () => {
+    const { model, onChange, content, ...rootProps } = this.props
+    const children = content.map(this.mapContentToChildren)
+    return <form {...rootProps}>{children}</form>
+  }
 }
 
-Form.defaultProps = {
-  content: [],
-}
-Form.propTypes = {
-  content: PropTypes.arrayOf(PropTypes.object)
+export const stateManager = updatedEl => prevState => {
+  const newModel = { ...prevState.model, ...updatedEl }
+  return { model: newModel }
 }
 
 export default Form
